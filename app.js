7f2554b3b88863385613788be27284aeb8184aac
MosNes <7hekarl@gmail.com>
2022-06-21 18:59:00 -0400

    added modularity

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 //gets the arguments provided to the command line, minus the first two args (which are just the file paths to node and the app itself)
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-//function that iterates through the array and console logs each item
-// .forEach is a method that only works on arrays
-const printProfileData = (profileDataArr) => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//gets the first two items in the profileDataArgs array and assigns them to name and github
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+//creates the html file, throws an error if there's a problem
+fs.writeFile('index.html',generatePage(name,github),err => {
+    if (err) throw err;
+
+    console.log('Portfolio Complete! Check out index.html to see the output!');
+});
