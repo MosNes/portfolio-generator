67511ea749c9d33145a543a8b70fc1e960c14530
MosNes <7hekarl@gmail.com>
2022-06-21 17:35:58 -0400

    final commit for capture-input

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+//gets the arguments provided to the command line, minus the first two args (which are just the file paths to node and the app itself)
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
+//function that iterates through the array and console logs each item
+// .forEach is a method that only works on arrays
 const printProfileData = (profileDataArr) => {
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
